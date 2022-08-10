214519e357ddec5562bd13f414f226af0089439f
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-11 01:54:37 +0500

    destructured the photos array and print data on screen

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [photos, setPhotos] = useState([]);
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
     url = `${mainUrl}${clientID}`;
-    console.log(url);
     setLoading(false);
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      setPhotos(data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
       setLoading(false);
@@ -27,6 +28,7 @@ function App() {
   useEffect(() => {
     fetchImages();
   }, []);
+
   return (
     <main>
       <section className="search">
@@ -39,25 +41,10 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos-center">
-          <article className="photo">
-            <img
-              src="https://images.unsplash.com/photo-1536329583941-14287ec6fc4e?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=MnwxNzY2ODF8MXwxfGFsbHwxfHx8fHx8Mnx8MTY2MDE1MTUwNA&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=1080"
-              alt="white notebook"
-            />
-            <div className="photo-info">
-              <div>
-                <h4>Keila Hötzel</h4>
-                <p>3308 likes</p>
-              </div>
-              <a href="http://www.theresponsiblecreatives.com">
-                <img
-                  src="https://images.unsplash.com/profile-1607516942909-bd6cc51a8fc7image?ixlib=rb-1.2.1&amp;crop=faces&amp;fit=crop&amp;w=64&amp;h=64"
-                  alt=""
-                  className="user-img"
-                />
-              </a>
-            </div>
-          </article>
+          {photos.map((photo) => {
+            const { id } = photo;
+            return <Photo key={photo.id} {...photo} />;
+          })}
         </div>
       </section>
     </main>
