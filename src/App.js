e1c10b22ccd1edfafaee376143c25496c0c11194
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-10 22:46:03 +0500

    setup Unsplash API and get data in console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+// const clientID = `?client_id=${process.env.Jt4hKqIPwd87B6OhzudgvlQpPekO6zsIVmpq47wJM7M}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}?client_id=Jt4hKqIPwd87B6OhzudgvlQpPekO6zsIVmpq47wJM7M`;
+    console.log(url);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
   return (
     <main>
       <section className="search">
