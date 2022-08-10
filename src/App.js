da6a9cf49b165bc44ea38f6d6cea10d3f45119cb
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-10 22:55:09 +0500

    setup environment variable for Access Key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Photo from './Photo';
-// const clientID = `?client_id=${process.env.Jt4hKqIPwd87B6OhzudgvlQpPekO6zsIVmpq47wJM7M}`;
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const fetchImages = async () => {
+    setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=Jt4hKqIPwd87B6OhzudgvlQpPekO6zsIVmpq47wJM7M`;
+    url = `${mainUrl}${clientID}`;
     console.log(url);
+    setLoading(false);
     try {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
@@ -28,24 +31,9 @@ function App() {
     <main>
       <section className="search">
         <form className="search-form">
-          <input
-            type="text"
-            placeholder="search"
-            className="form-input"
-            value=""
-          />
+          <input type="text" placeholder="search" className="form-input" />
           <button type="submit" className="submit-btn">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 512 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-            </svg>
+            <FaSearch />
           </button>
         </form>
       </section>
